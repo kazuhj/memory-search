6665e3c35a5ed6ae48b908c4675e04b053b683ae
kazuya hojo <maboai.kazu29@gmail.com>
2020-04-12 11:52:48 +0900

    add/application_controller編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth, if: :production?
+
+  private
+
+  def production?
+    Rails.env.production?
+  end
+  
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["MEMOBASIC_AUTH_USER"] && password == ENV["MEMOBASIC_AUTH_PASSWORD"]
+    end
+  end
 end
