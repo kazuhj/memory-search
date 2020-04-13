9d05d0e876521ac1f7532655698703168b6ce09e
kazuya hojo <maboai.kazu29@gmail.com>
2020-04-13 11:08:58 +0900

    add/mypageコントローラー編集
    投稿情報を追加

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -2,6 +2,8 @@ class MypagesController < ApplicationController
   before_action :set_group
 
   def index
+    @contents = Content.where(user_id: current_user.id).order("id DESC").limit(5)
+    @content = Content.where(user_id: current_user.id)
   end
 
   private
