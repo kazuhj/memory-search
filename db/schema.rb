d4d7cab66ee0fb90e04cb1abdb74b0bf24f4234a
kazuya hojo <maboai.kazu29@gmail.com>
2020-04-13 10:49:23 +0900

    add/gem 'jquery-rails'導入

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_12_110455) do
+ActiveRecord::Schema.define(version: 2020_04_12_145049) do
+
+  create_table "contents", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "text"
+    t.text "title", null: false
+    t.string "image"
+    t.bigint "user_id", null: false
+    t.bigint "group_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_contents_on_group_id"
+    t.index ["user_id"], name: "index_contents_on_user_id"
+  end
 
   create_table "group_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "group_id"
@@ -41,6 +53,8 @@ ActiveRecord::Schema.define(version: 2020_04_12_110455) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "contents", "groups"
+  add_foreign_key "contents", "users"
   add_foreign_key "group_users", "groups"
   add_foreign_key "group_users", "users"
 end
