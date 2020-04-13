7d3ca527b257956605e609b796aeddce451a71eb
kazuya hojo <maboai.kazu29@gmail.com>
2020-04-13 17:29:37 +0900

    add/content.rb編集
    検索条件を設定
    タイトル,メモ,投稿者で検索できるように設定

--- app/models/content.rb
+++ app/models/content.rb
@@ -3,4 +3,9 @@ class Content < ApplicationRecord
   belongs_to :group
   validates :title, presence: true, unless: :image?
   mount_uploader :image, ImageUploader
+
+  def self.search(search)
+    return Content.all unless search
+    Content.where(['title LIKE ? OR text LIKE ? OR @user.name LIKE ?', "%#{search}%", "%#{search}%", "%#{search}%"])
+  end
 end
