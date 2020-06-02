b73e019c5d66052484265b8319da905c676e6ddd
kazuya hojo <maboai.kazu29@gmail.com>
2020-06-02 16:42:04 +0900

    add/groups:name index_delete
    グループ名のインデックスと一意性制約を削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_13_142026) do
+ActiveRecord::Schema.define(version: 2020_06_02_031328) do
 
   create_table "contents", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "text"
@@ -37,7 +37,6 @@ ActiveRecord::Schema.define(version: 2020_04_13_142026) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_groups_on_name", unique: true
   end
 
   create_table "taggings", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
